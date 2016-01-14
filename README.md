91ed53bd49cafefc5fb7e287f1eec09f4edadd85
lukaszm-rc <lukasz.mazurek@redcart.pl>
2016-01-14 16:15:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Aurelia sample app
+
+## Depedencies
+- npm
+- jspm
+
+## Usage
+ Just clone the repository, and then
+ ```
+ npm install
+ jspm install
+ ```
+ 
+## Deploying app
+ ```
+ gulp bundle
+ ```
+
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import {Router} from "aurelia-router";
+
+export class App {
+	static inject() {
+		return [Router];
+	}
+
+	message = "Welcome to Aurelia";
+
+	constructor(router) {
+		this.router = router;
+		this.router.configure(config => {
+			config.title = 'Aurelia';
+			config.map([{
+				route : ["","welcome"], // Activate this route by default or when on /foo
+				moduleId : "welcome", // When active, load foo.js and foo.html (module)
+				name : "welcome",
+				nav : true, // Add this route to the list of navigable routes (used for building UI)
+				title : "Welcome" // Used in the creation of a pages title
+			}]);
+
+			this.router = router;
+		});
+	}
+}
